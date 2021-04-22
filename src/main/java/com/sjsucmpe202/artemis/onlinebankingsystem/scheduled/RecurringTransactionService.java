bc4989dab038ce44b708c0bb0aea47c08e224220
Jeena <jeenathampi.23@gmail.com>
2021-04-22 15:25:59 -0700

    Added more logs

--- src/main/java/com/sjsucmpe202/artemis/onlinebankingsystem/scheduled/RecurringTransactionService.java
+++ src/main/java/com/sjsucmpe202/artemis/onlinebankingsystem/scheduled/RecurringTransactionService.java
@@ -7,16 +7,18 @@ import com.sjsucmpe202.artemis.onlinebankingsystem.entities.accounts.BankAccount
 import com.sjsucmpe202.artemis.onlinebankingsystem.mappers.TransactionMapper;
 import com.sjsucmpe202.artemis.onlinebankingsystem.repositories.TransactionTemplateRepository;
 import com.sjsucmpe202.artemis.onlinebankingsystem.services.TransactionService;
+import lombok.extern.slf4j.Slf4j;
+import org.codehaus.plexus.util.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 
 @Service
+@Slf4j
 public class RecurringTransactionService {
 
     private TransactionTemplateRepository transactionTemplateRepository;
@@ -33,25 +35,35 @@ public class RecurringTransactionService {
     }
 
     @Scheduled(cron = "0 0 0 * * *", zone = "America/Los_Angeles")
-    @Transactional
     public void createRecurringTransaction() {
         //Get the list of transaction templates for the current date
         Iterable<TransactionTemplate> transactionTemplates = transactionTemplateRepository.findAllByNextTriggerDateEquals(LocalDate.now());
 
         //Iterate over each template
         for(TransactionTemplate template: transactionTemplates){
-            BankAccount fromAccount = template.getFromBankAccount();
-            BankAccount toAccount = template.getToBankAccount();
-
-            //Create TXN
-            Transaction fromTxn = transactionMapper.toTransactionDTOForFromAccount(template);
-            Transaction toTxn = transactionMapper.toTransactionDTOForToAccount(template);
-            transactionService.save(fromTxn, fromAccount.getId());
-            transactionService.save(toTxn, toAccount.getId());
-
-            // Update Next Trigger Date;
-            template.setNextTriggerDate(transactionUtil.calculateNextTriggerDate(template));
-            transactionTemplateRepository.save(template);
+            try{
+                saveRecurringTransaction(template);
+                log.info("Successfully saved the transaction");
+            }
+            catch (Exception e){
+                log.error("Exception occured while saving this transaction. e={}", ExceptionUtils.getStackTrace(e));
+            }
         }
+    }
+
+    @Transactional
+    public void saveRecurringTransaction(TransactionTemplate template) {
+        BankAccount fromAccount = template.getFromBankAccount();
+        BankAccount toAccount = template.getToBankAccount();
+
+        //Create TXN
+        Transaction fromTxn = transactionMapper.toTransactionDTOForFromAccount(template);
+        Transaction toTxn = transactionMapper.toTransactionDTOForToAccount(template);
+        transactionService.save(fromTxn, fromAccount.getId());
+        transactionService.save(toTxn, toAccount.getId());
+
+        // Update Next Trigger Date;
+        template.setNextTriggerDate(transactionUtil.calculateNextTriggerDate(template));
+        transactionTemplateRepository.save(template);
     }
 }
